e41e60df415409ad2881f78ec08721ba6806265e
Yuliya <fokju@yandex.ru>
2022-10-10 17:51:00 +0300

    znach

---
+++ strings.py
@@ -0,0 +1,15 @@
+BOT_TOKEN_FILENAME = 'token.txt'
+
+#символы
+SYMBOL_X = 'X'
+SYMBOL_O = 'O'
+SYMBOL_UNDEF = '*'
+
+#ответы бота
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Вы победили'
+ANSW_BOT_WIN = 'Бот победил'
+ANSW_DRAW = 'Ничья'
+ANSW_HELP = 'Используйте команду /new_game для начала новой игры'
+
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать только на ', SYMBOL_UNDEF
